47a894763f83f69ddff4632d567b88db65eff2d7
Yasser <brdyasser@gmail.com>
2023-01-30 10:01:26 +0100

    event

---
+++ event/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import ClickEvent from './components/ClickEvent';
+
+
+class App extends Component{
+
+  render (){
+     return (
+      <ClickEvent/>
+     )
+  }
+
+ 
+
+}
+export default App;
---
+++ event/src/components/ClickEvent.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+function ClickEvent() {
+
+  function clickHandler(e){
+    e.preventDefault();
+    console.log('button click')
+  }
+
+
+  return (
+    <div >
+        <form>
+           <button onClick={clickHandler}> click</button>
+        </form>
+
+        <form>
+
+        <button> hello</button>
+
+        </form>
+
+    </div>
+  )
+}
+
+export default ClickEvent
