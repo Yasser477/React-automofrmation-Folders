a958c523a14dea384f8b6bb55c8836e2d3632b48
Yasser <brdyasser@gmail.com>
2023-01-30 10:04:10 +0100

    conditionals

---
+++ conditionals/src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import userG from "./components/userG";
+
+
+
+class  App extends Component {
+ 
+render(){
+    return(
+        <div className="App" >
+            <userG />
+        </div>
+    )
+}
+}
+
+export default App;
---
+++ conditionals/src/components/userG.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+class userG extends Component {
+  render() {
+    return (
+      
+      <div> welcome all </div>
+    )
+  }
+}
+
+export default userG
