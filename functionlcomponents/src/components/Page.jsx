b62e96f88ead1322bfb9682b580c7b2e1614ba2c
Yasser <brdyasser@gmail.com>
2023-01-30 10:02:35 +0100

    functionalComponent

---
+++ functionlcomponents/src/components/Page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+// function Simple() {
+//     return <h1> Hello </h1>;
+// }
+
+
+const Simpletwo = () => <h1> hello world </h1>
+
+
+// export default Simple;
+
+export default Simpletwo;
---
+++ functionlcomponents/src/components/message.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import MyComponent from "./components/Page"
+
+class App extends Component{
+
+  render(){
+    return(
+      <div className="App">
+
+
+        <MyComponent/>
+
+
+      </div>
+    )
+  }
+
+}
+
+export default App;
